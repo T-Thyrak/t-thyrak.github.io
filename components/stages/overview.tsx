61ef0637a56cbd65e41110f098004b365f5557c9
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2024-04-06 21:57:07 +0700

    quoting issue

--- components/stages/overview.tsx
+++ components/stages/overview.tsx
@@ -6,66 +6,63 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 
 export default function OverviewStage() {
-    return (
-        <div className="flex flex-col items-center justify-center h-full w-full my-8">
-            <h1 className="text-5xl font-bold m-2" id="overview">
-                Overview
-            </h1>
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full my-8">
+      <h1 className="text-5xl font-bold m-2" id="overview">
+        Overview
+      </h1>
 
-            <hr className="w-full keep-border border-rosewater" />
+      <hr className="w-full keep-border border-rosewater" />
 
-            <div className="flex space-x-6 h-full w-full border-2 border-white">
-                <FadeInObject className="flex w-full h-full -translate-x-4">
-                    <div className="flex items-center justify-center h-full w-full my-4">
-                        <Image
-                            src="/images/portrait-no-bg.png"
-                            width={300}
-                            height={300}
-                            alt="portrait"
-                            // className="rounded-xl border-2 border-teal keep-border"
-                        />
-                    </div>
-                </FadeInObject>
+      <div className="flex space-x-6 h-full w-full border-2 border-white">
+        <FadeInObject className="flex w-full h-full -translate-x-4">
+          <div className="flex items-center justify-center h-full w-full my-4">
+            <Image
+              src="/images/portrait-no-bg.png"
+              width={300}
+              height={300}
+              alt="portrait"
+              // className="rounded-xl border-2 border-teal keep-border"
+            />
+          </div>
+        </FadeInObject>
 
-                <FadeInObject className="flex w-full translate-x-4 border-2 border-green">
-                    <div className="flex flex-col items-center justify-center h-full w-full border-2 border-blue">
-                        <h2 className="text-3xl my-4">
-                            Hi, I am{" "}
-                            <span className="font-bold">Teng Thaisothyrak</span>
-                            .
-                        </h2>
-                        <div className="flex items-center justify-center w-full h-full border-2 border-rosewater">
-                            <div className="flex flex-col justify-center items-start w-full h-full border-2 border-green">
-                                <p>
-                                    I am primarily a backend developer with a
-                                    passion for machine learning and data
-                                    science, but I can also do web development
-                                    as well as mobile development.
-                                </p>
-                                <br />
-                                <p>
-                                    I have experience in developing web
-                                    applications using Flask, and aiohttp.
-                                </p>
-                                <br />
-                                <p>
-                                    I am also qualified for Cisco's
-                                    CyberOperations Associate.
-                                </p>
-                            </div>
-                        </div>
-
-                        <button className="m-4 px-4 py-1 rounded-xl border-2 border-sapphire keep-border bg-surface0 hover:bg-surface1">
-                            <div className="space-x-2">
-                                <span>
-                                    <FontAwesomeIcon icon={faFilePdf} />
-                                </span>
-                                <span>Get my CV!</span>
-                            </div>
-                        </button>
-                    </div>
-                </FadeInObject>
+        <FadeInObject className="flex w-full translate-x-4 border-2 border-green">
+          <div className="flex flex-col items-center justify-center h-full w-full border-2 border-blue">
+            <h2 className="text-3xl my-4">
+              Hi, I am <span className="font-bold">Teng Thaisothyrak</span>.
+            </h2>
+            <div className="flex items-center justify-center w-full h-full border-2 border-rosewater">
+              <div className="flex flex-col justify-center items-start w-full h-full border-2 border-green">
+                <p>
+                  I am primarily a backend developer with a passion for machine
+                  learning and data science, but I can also do web development
+                  as well as mobile development.
+                </p>
+                <br />
+                <p>
+                  I have experience in developing web applications using Flask,
+                  and aiohttp.
+                </p>
+                <br />
+                <p>
+                  I am also qualified for Cisco&quot;s CyberOperations
+                  Associate.
+                </p>
+              </div>
             </div>
-        </div>
-    );
+
+            <button className="m-4 px-4 py-1 rounded-xl border-2 border-sapphire keep-border bg-surface0 hover:bg-surface1">
+              <div className="space-x-2">
+                <span>
+                  <FontAwesomeIcon icon={faFilePdf} />
+                </span>
+                <span>Get my CV!</span>
+              </div>
+            </button>
+          </div>
+        </FadeInObject>
+      </div>
+    </div>
+  );
 }
