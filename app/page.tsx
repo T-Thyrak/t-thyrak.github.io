9b620c7e0db4c2eb5970c642c2d44e9118fc36dd
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2024-04-06 21:59:51 +0700

    export issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,39 +7,39 @@ import { StickyButton } from "@/components/sticky";
 import UnderConstruction from "@/components/under_construction";
 
 export default function Home() {
-    const underConstruction = false;
-    return (
-        <main className="flex min-h-screen justify-start flex-col px-16 py-8 text-lg">
-            {underConstruction ? <UnderConstruction /> : <Page />}
-        </main>
-    );
+  const underConstruction = false;
+  return (
+    <main className="flex min-h-screen justify-start flex-col px-16 py-8 text-lg">
+      {underConstruction ? <UnderConstruction /> : <Page />}
+    </main>
+  );
 }
 
 function Page() {
-    return (
-        <div>
-            <Navbar />
-            <Content />
-            <StickyButton />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Content />
+      <StickyButton />
+    </div>
+  );
 }
 
-export function Content() {
-    return (
-        <div>
-            <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
-                <OverviewStage />
-            </FadeInObject>
-            <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
-                <ExperiencesStage />
-            </FadeInObject>
-            <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
-                <SkillsStage />
-            </FadeInObject>
-            <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
-                <UnderConstruction />
-            </FadeInObject>
-        </div>
-    );
+function Content() {
+  return (
+    <div>
+      <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
+        <OverviewStage />
+      </FadeInObject>
+      <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
+        <ExperiencesStage />
+      </FadeInObject>
+      <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
+        <SkillsStage />
+      </FadeInObject>
+      <FadeInObject className="flex flex-col items-center justify-center w-full h-full translate-y-4">
+        <UnderConstruction />
+      </FadeInObject>
+    </div>
+  );
 }
